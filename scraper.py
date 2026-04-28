ca319b45cbe3dcfd19ba7b355d5989c64db87fac
sivasubramoniam-js <122598293+sivasubramoniam-js@users.noreply.github.com>
2026-04-28 13:53:09 +0530

    added logic to extract 50 items from the list

--- scraper.py
+++ scraper.py
@@ -169,7 +169,7 @@ def phase1_fetch_feed(feed):
         if new_fetched_items:
             temp_path = f"{TEMP_DIR}/{slugify(feed_name)}.json"
             with open(temp_path, 'w', encoding='utf-8') as f:
-                json.dump(new_fetched_items, f, indent=2)
+                json.dump(new_fetched_items[:50], f, indent=2)
             print(f"Phase 1 - Saved {len(new_fetched_items)} new temp items to {feed_name}")
             
     except Exception as e: 
